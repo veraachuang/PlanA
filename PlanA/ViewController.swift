dde118cbb416166e24f7c0e33a9f8b7418c2fa5e
vera chuang <verachuanghh0809@gmail.com>
2022-08-03 22:42:00 +0800

    Initial Commit

---
+++ PlanA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlanA
+//
+//  Created by vera chuang on 3/8/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
